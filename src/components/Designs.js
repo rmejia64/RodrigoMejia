ec0be9982a4a3694cae737fa89708ef0d1edc37c
Rodrigo Mejia <128569752+rmejia64@users.noreply.github.com>
2024-02-26 08:20:13 -0800

    added 'designs' and 'projects' section

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -1,10 +1,27 @@
 import "../styles/Designs.css";
 
+import design1 from "../assets/Bill_Wilson_Center_Redesign.png";
+
 function Designs() {
 	return (
-		<div className="content">
-			<p className="title">Designs</p>
+		<div className="content-two">
+			<p className="title-two">Designs</p>
 			<div className="cards">
+				<img
+					className="design"
+					alt="Redesign of Bill Wilson Center website"
+					src={design1}
+				/>
+				<img
+					className="design"
+					alt="Redesign of SCU Bowling Club website"
+					src={require("../assets/SCU_Bowling_Club.png")}
+				/>
+				<img
+					className="design"
+					alt="Redesign of Semillas De Amor website"
+					src={require("../assets/Semillias_Website.png")}
+				/>
 			</div>
 		</div>
 	);
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,50 @@ import "../styles/Projects.css";
 
 function Projects() {
 	return (
-		<div className="content">
-			<p className="title">Projects</p>
-			<div className="cards">
+		<div className="content-two">
+			<p className="title-two">Projects</p>
+			<div className="cards-two">
+				<a
+					className="project-link"
+					target="_blank"
+					rel="noreferrer"
+					href="https://drinkraja.com"
+				>
+					<img
+						className="project"
+						alt="Drink Raja"
+						src={require("../assets/raja-logo.png")}
+					/>
+					<br></br>A preliminary webite for Raja Chai Latte.
+				</a>
+				<a
+					className="project-link"
+					target="_blank"
+					rel="noreferrer"
+					href="https://zepedadogs.web.app"
+				>
+					<img
+						className="project"
+						alt="Zepeda Dog Services"
+						src={require("../assets/zepeda.png")}
+					/>
+					<br></br>The official website for Zepeda Dog Services. A Dog training, walking,
+					and daycare service.
+				</a>
+				<a
+					className="project-link"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.zaragozasconstruction.com"
+				>
+					<img
+						className="project"
+						alt="Zaragoza Construction"
+						src={require("../assets/zaragoza.png")}
+					/>
+					<br></br>The website for Zaragoza Construction. A local home rennovation
+					service.
+				</a>
 			</div>
 		</div>
 	);
